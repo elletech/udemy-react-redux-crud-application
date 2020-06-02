57c33bc82517aef952ed5b91c77484c78921ea85
須加尾勇太 <elletech@sugabiyuutanoMacBook-Pro.local>
2020-06-02 10:52:22 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
-
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => {console.log("I am clicked")}}></input>
-//     </React.Fragment>
-//   )
-// }
-
-// class App extends Component {
-//   render() {
-//     return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => {console.log("I am clicked")}}></input>
-//     </React.Fragment>
-//   )
-//   }
-// }
-
+import React from 'react';
 const App = () => {
+  const profiles = [
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 5},
+    {name: "noName"}
+  ]
   return (
   <div>
-    <Cat />
-    <Cat />
-    <Cat />
+    {
+      // keyが必要なのでindexを追加している
+      profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
   </div>
   )
 }
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+return <div>Hi! I am {props.name}, and {props.age} years old! </div>
 }
 
+// デフォルトPropsの設定
+User.defaultProps = {
+  age: 1
+}
 export default App;
